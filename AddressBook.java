354e5080cc5cc23e4eaa93bc66e3da07b9baf0cc
Praphulla Kherade <prafullkherade9@gmail.com>
2021-11-01 16:54:47 +0530

    Add a new contact in address book

--- AddressBook.java
+++ AddressBook.java
@@ -1,36 +1,71 @@
 package com.addressbook;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class AddressBook {
 
-    private class Contact {
-        String firstName, lastName, address, city, state, emailId;
-        int zipCode;
-        long mobileNumber;
+    ArrayList arrayList = new ArrayList();
+    ContactDetails person = new ContactDetails();
+    public void addNewContact() {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter First Name : ");
+        String firstName = scanner.next();
+        System.out.println("Enter Last Name : ");
+        String lastName = scanner.next();
+        System.out.println("Enter Address : ");
+        String address = scanner.next();
+        System.out.println("Enter City : ");
+        String city = scanner.next();
+        System.out.println("Enter State : ");
+        String state = scanner.next();
+        System.out.println("Enter ZipCode : ");
+        int zipCode = scanner.nextInt();
+        System.out.println("Enter Mobile Number : ");
+        long mobileNumber = scanner.nextLong();
+        System.out.println("Enter EmailId : ");
+        String emailId = scanner.next();
+        person = new ContactDetails(firstName, lastName, address, city, state, zipCode, mobileNumber, emailId);
+        arrayList.add(person);
+        printContact();
     }
 
     public void printContact() {
-        Contact person = new Contact();
-        person.firstName = "Praphulla";
-        person.lastName = "Kherade";
-        person.address = "Bhandup";
-        person.city = "Mumbai";
-        person.state = "Maharashtra";
-        person.zipCode = 407478;
-        person.mobileNumber = 8542157565L;
-        person.emailId = "prafullkherade9gmail.com";
-        System.out.println("Contact Details");
-        System.out.println("Name         : "  + person.firstName + " " + person.lastName + "\n"
-                + "Address      : "  + person.address + "\n"
-                + "City         : "  + person.city + "\n"
-                + "State        : "  + person.state + "\n"
-                + "ZipCode      : "  + person.zipCode + "\n"
-                + "MobileNumber : "  + person.mobileNumber + "\n"
-                + "EmailId      : "  + person.emailId + "\n" );
+
+        for(int i=0;i<arrayList.size();i++) {
+
+            System.out.println("Contact Details");
+            System.out.println("Name         : " + person.getFirstName() + " " + person.getLastName() + "\n"
+                    + "Address      : " + person.getAddress()   + "\n"
+                    + "City         : " + person.getCity()      + "\n"
+                    + "State        : " + person.getState()     + "\n"
+                    + "ZipCode      : " + person.getZipCode()   + "\n"
+                    + "MobileNumber : " + person.getMobileNumber()  + "\n"
+                    + "EmailId      : " + person.getEmailId()   + "\n");
+        }
     }
 
     public static void main(String[] args) {
+        System.out.println("Welcome to Address Book Program");
+        Scanner userInput = new Scanner(System.in);
         AddressBook addressBook = new AddressBook();
-        addressBook.printContact();
+        while(true){
+
+            System.out.println("Enter 1 to add new contact \nEnter 2 to Exit");
+            int getUserInput = userInput.nextInt();
+            switch (getUserInput){
+                case 1 :
+                    addressBook.addNewContact();
+                    break;
+                case 2 :
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Invalid user input");
+                    continue;
+            }
+        }
     }
 
 }
---
+++ ContactDetails.java
@@ -0,0 +1,96 @@
+package com.addressbook;
+
+public class ContactDetails {
+    //variables
+    protected String firstName;
+    protected String lastName;
+    protected String address;
+    protected String city;
+    protected String state;
+    protected int zipCode;
+    protected long mobileNumber;
+    protected String emailId;
+
+    //Constructor
+    public ContactDetails(){
+
+    }
+    public ContactDetails(String firstName, String lastName, String address, String city,
+                          String state, int zipCode, long mobileNumber, String emailId){
+        this.firstName=firstName;
+        this.lastName=lastName;
+        this.address=address;
+        this.city=city;
+        this.state=state;
+        this.zipCode=zipCode;
+        this.mobileNumber=mobileNumber;
+        this.emailId=emailId;
+
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public int getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(int zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public long getMobileNumber() {
+        return mobileNumber;
+    }
+
+    public void setMobileNumber(long mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+
+}
