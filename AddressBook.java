4318c02573263fa94bb50f13bd30c486b1414fe6
Praphulla Kherade <prafullkherade9@gmail.com>
2021-11-01 16:49:48 +0530

    Create a contact in address book

--- AddressBook.java
+++ AddressBook.java
@@ -2,7 +2,35 @@ package com.addressbook;
 
 public class AddressBook {
 
-    public static void main(String[] args) {
-        System.out.println("Welcome to Address Book System");
+    private class Contact {
+        String firstName, lastName, address, city, state, emailId;
+        int zipCode;
+        long mobileNumber;
     }
+
+    public void printContact() {
+        Contact person = new Contact();
+        person.firstName = "Praphulla";
+        person.lastName = "Kherade";
+        person.address = "Bhandup";
+        person.city = "Mumbai";
+        person.state = "Maharashtra";
+        person.zipCode = 407478;
+        person.mobileNumber = 8542157565L;
+        person.emailId = "prafullkherade9gmail.com";
+        System.out.println("Contact Details");
+        System.out.println("Name         : "  + person.firstName + " " + person.lastName + "\n"
+                + "Address      : "  + person.address + "\n"
+                + "City         : "  + person.city + "\n"
+                + "State        : "  + person.state + "\n"
+                + "ZipCode      : "  + person.zipCode + "\n"
+                + "MobileNumber : "  + person.mobileNumber + "\n"
+                + "EmailId      : "  + person.emailId + "\n" );
+    }
+
+    public static void main(String[] args) {
+        AddressBook addressBook = new AddressBook();
+        addressBook.printContact();
+    }
+
 }
