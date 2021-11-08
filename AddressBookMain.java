24174c2460d30f8ddc166ba64c7135b08a80a5b9
Praphulla Kherade <prafullkherade9@gmail.com>
2021-11-08 21:58:24 +0530

    sort person details alphabetically

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -1,8 +1,55 @@
 package com.stream;
 
+import java.util.Scanner;
+
 public class AddressBookMain {
+    private static Scanner scan = new Scanner(System.in);
 
     public static void main(String[] args) {
-        System.out.println("Welcome to adreess book system");
+        // Displaying Welcome Message
+        System.out.println("Welcome to Address Book Program");
+        // Creating an object of Class AddressBook
+        ContactDetails addressBook = new ContactDetails();
+        // Calling methods from class AddressBookRunner
+        boolean choice = true;
+        while (choice) {
+            System.out.println("\nChoose the option:\nPress '1' for Add Contact Details\nPress '2' for Edit Contact Deatils"
+                    + "\nPress '3' for Delete Contact Details\nPress '4' for Show Contact Details\nPress '5' for Contact list by City State"
+                    + "\nPress '6' for Show Contact list of Persons\nPress '7' for Count of contacts by City State\nPress '8' for Sort Contacts By Name"
+                    + "\nPress '0' for Exit");
+            int option = scan.nextInt();
+            switch (option) {
+                case 0:
+                    choice = false;
+                    System.out.println("Exited out of AddressBook");
+                    break;
+                case 1:
+                    addressBook.addDetails();
+                    break;
+                case 2:
+                    addressBook.editDetails();
+                    break;
+                case 3:
+                    addressBook.deleteDetails();
+                    break;
+                case 4:
+                    addressBook.showDetails();
+                    break;
+                case 5:
+                    addressBook.contactBycity();
+                    break;
+                case 6:
+                    addressBook.displayContacts();
+                    break;
+                case 7:
+                    addressBook.contactsCount();
+                    break;
+                case 8:
+                    addressBook.sortByName();
+                    break;
+                default:
+                    System.out.println("Please enter valid input");
+            }
+        }
     }
 }
